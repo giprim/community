946b8962348e61a8ee9d58f3c25f11e1537f4dd6
Giprim <gi4real.gi@gmail.com>
2020-10-24 13:16:43 +0100

    feat: implement banner design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import React from "react";
+import "./App.css";
+import Banner from "./components/Banner";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledFluidContainer>
+      <StyledContainer>
+        <Banner />
+      </StyledContainer>
+    </StyledFluidContainer>
   );
 }
 
 export default App;
+
+const StyledContainer = styled("div")`
+  margin: 0 auto;
+  max-width: 380px;
+  @media (min-width: 700px) {
+    max-width: 520px;
+  }
+  @media (min-width: 1000px) {
+    max-width: 1050px;
+  }
+`;
+
+const StyledFluidContainer = styled("div")`
+  width: 100%;
+`;
